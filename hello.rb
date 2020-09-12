456dd46c452b5dbbbfd386e3fbbd8f9d581883d6
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-09-12 13:19:18 +0200

    First Commit

---
+++ hello.rb
@@ -0,0 +1,19 @@
+class Employee
+  def setname(name)
+    @name = name
+  end
+
+  def getname
+    puts(@name)
+  end 
+end
+
+
+e = Employee.new
+e.setname('Abdo')
+e.getname
+b =  "I am here"
+c = 123
+
+
+
